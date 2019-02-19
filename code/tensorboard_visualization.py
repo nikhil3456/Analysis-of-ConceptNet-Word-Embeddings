3c86e045189ee494e323e256b59eb27fb0a01ff1
Nikhil Bansal <nikhilbansal3456@gmail.com>
2019-02-20 00:47:17 +0530

    Added markdown in file tensorboard_visualization.ipynb

--- code/tensorboard_visualization.py
+++ code/tensorboard_visualization.py
@@ -13,6 +13,13 @@ import tensorflow as tf
 from tensorflow.contrib.tensorboard.plugins import projector
 
 
+# ### Few Resources that I used for concept-Net's word-embeddings 'NumberBatch'
+# - Github Repo: https://github.com/commonsense/conceptnet-numberbatch
+# - PreTrained word-embeddings Download link: https://conceptnet.s3.amazonaws.com/downloads/2017/numberbatch/numberbatch-17.06.txt.gz
+# 
+# ### Link used for implementing Tensorboard visualization of this word embeddings
+# - https://stackoverflow.com/questions/50492676/visualize-gensim-word2vec-embeddings-in-tensorboard-projector
+
 # In[29]:
 
 
